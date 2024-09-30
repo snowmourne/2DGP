6ef109d7c1e506c790a6eb48b2dafadb8e6b30e8
snowmourne_kpu <seolhan7@gmail.com>
2024-09-30 12:18:21 +0900

    실습내용

--- Lecture06_HandlingInputs/move_character_with_key.py
+++ Lecture06_HandlingInputs/move_character_with_key.py
@@ -7,23 +7,33 @@ character = load_image('animation_sheet.png')
 
 
 def handle_events():
-    global running
-
-    # fill here
-
+    global running, dir
+    global x
     events = get_events()
+
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        # fill here
+        elif event.key == SDLK_RIGHT:
+            x += 10
+        elif event.key == SDLK_LEFT:
+            x -= 10
+        elif event.key == SDLK_ESCAPE:
+            running = False
 
 
 running = True
 x = 800 // 2
 frame = 0
 
-# fill here
-
+while running:
+    clear_canvas()
+    grass.draw(400,30)
+    character.clip_draw(frame * 100, 100, 100, 100, x, 90)
+    update_canvas()
+    handle_events()
+    frame = (frame+1) % 8
+    delay(0.05)
 
 close_canvas()
 
